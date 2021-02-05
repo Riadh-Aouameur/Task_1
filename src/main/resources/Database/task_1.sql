846c41a318b315e384cbf2bdb64528202db981c8
Riadh-Aouameur <riadhaouameur02@gmail.com>
2021-02-05 21:50:31 +0100

    clean the code

--- src/main/resources/Database/task_1.sql
+++ src/main/resources/Database/task_1.sql
@@ -2,7 +2,7 @@ DROP DATABASE IF EXISTS task_1;
 create database task_1;
 use task_1 ;
 CREATE TABLE T1 (
-id INT(6) AUTO_INCREMENT not null,
+id  VARCHAR(10) NOT NULL,
 projectType VARCHAR(32) NOT NULL,
 atribueX VARCHAR(32) NOT NULL,
 iDescription VARCHAR(128) NOT NULL,
